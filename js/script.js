1af293a1707ec7461ffb6f7a30d72512ec5a01c5
carlosgdaj <carlosjunior1104jr@gmail.com>
2021-06-29 01:04:40 -0300

    versao-final-interface-netflix

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1.5
+        },
+        360:{
+            items:2.1
+        },
+        500:{
+            items:3
+        },
+        600:{
+            items:3.5
+        },
+        800:{
+            items:4
+        },
+        900:{
+            items:5
+        },
+        
+    }
+})
